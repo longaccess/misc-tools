05a60fe736c603f591ae6d6ca7c8c7de35f23fa4
Panayotis Vryonis <vrypan@gmail.com>
2013-10-31 00:51:40 +0200

    decrypt.py reads in 16KB chunks from STDIN
    Instead or reading the whole source file in memory
    and decrypting, now decrypt.py reads data in 16KB chunks.

--- encryption/decrypt.py
+++ encryption/decrypt.py
@@ -5,16 +5,21 @@
 
 import Crypto.Cipher.AES
 import Crypto.Util.Counter
-from base64 import b64decode
+from base64 import b64decode, b16decode
 import sys
 
-key = b64decode(sys.argv[1])
-encrypted = sys.stdin.read()
+key = b16decode(sys.argv[1], True)
 
 BLOCKSIZE = 16
 ctr = Crypto.Util.Counter.new(128, prefix='', initial_value=0L)
 cipher = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CTR, counter=ctr)
-decrypted = cipher.decrypt(encrypted)
-num = ord(decrypted[-1])
 
-sys.stdout.write(decrypted[0:-num])
+next_chunk = ''
+finished = False
+while not finished:
+    chunk, next_chunk = next_chunk, cipher.decrypt(sys.stdin.read(1024 * BLOCKSIZE))
+    if len(next_chunk) == 0:
+        padding_length = ord(chunk[-1])
+        chunk = chunk[:-padding_length]
+        finished = True
+    sys.stdout.write(chunk)
